461018e94f9df38ac1062b61f91dfd2b019ffd59
TJMusiitwa <jonamusiitwa@outlook.com>
2023-06-15 10:44:12 +0100

    Remove uneeded import

--- test/adaptable_scaffold_test.dart
+++ test/adaptable_scaffold_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:adaptable_scaffold/adaptable_scaffold.dart';
-
 void main() {
   test('adds one to input values', () {
     // final calculator = Calculator();
